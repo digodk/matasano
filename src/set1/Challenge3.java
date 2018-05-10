5afd0159dd04851cab535eb463e99e9a96122a2a
digodk <diogodklein@gmail.com>
2018-05-09 22:04:52 -0300

    Challenge3 on the way

--- src/set1/Challenge3.java
+++ src/set1/Challenge3.java
@@ -1,7 +1,9 @@
 package set1;
 
+import java.util.Arrays;
+
 /*
- * The hex  encoded string:
+ * The hex encoded string:
  * 
  * 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
  * ... has been XOR'd against a single character. Find the key, decrypt the message.
@@ -12,14 +14,62 @@ package set1;
  */
 public class Challenge3 {
 
+  static public Letter[] stdCharsFrequencies = new Letter[26];
+  static public Letter[] emptyCharsFrequencies = new Letter[26];
+  static public String chars = "etaoinshrdlcumwfgypbvkjxqz";
+  static public double[] frequencies = { 12.702, 9.056, 8.167, 7.507, 6.966, 6.749, 6.327, 6.094,
+      5.987, 4.253, 4.025, 2.782, 2.758, 2.406, 2.36, 2.228, 2.015, 1.974, 1.929, 1.492, 0.978,
+      0.772, 0.153, 0.15, 0.095, 0.074 };
+
+  class Letter {
+    String name;
+    double frequency;
+    int counter = 0;
+
+    Letter(char name, double frequency) {
+      this.name = Character.toString(name);
+      this.frequency = frequency;
+    }
+
+    void count() {
+      counter++;
+    }
+  }
+
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
   }
-  
+
+  void initLetterTables() {
+    for (int ix = 0; ix < chars.length(); ix++) {
+      stdCharsFrequencies[ix] = new Letter(chars.charAt(ix), frequencies[ix]);
+      emptyCharsFrequencies[ix] = new Letter(chars.charAt(ix), 0);
+    }
+  }
+
+  // Evaluates how probable a given is to be a valid english text. Measures both
+  // letter frequency and also word length.
   static double evaluator(String s) {
-    
+
     return 0;
+  }
+
+  Letter[] freqList(String s) {
+    Letter[] list = Arrays.copyOf(emptyCharsFrequencies, emptyCharsFrequencies.length);
+    int position = 0;
+    for (Character c : s.toCharArray()) {
+      position = chars.indexOf(c);
+      list[position].count();
+    }
+    for (Letter letter : list) {
+      letter.frequency = letter.counter / ((double) s.length()) * 100;
+    }
+    return list;
+  }
+
+  String hexStrToStr(String hex) {
+
+    return null;
   }
 
 }
